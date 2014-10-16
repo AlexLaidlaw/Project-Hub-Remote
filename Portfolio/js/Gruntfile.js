50694cf8da33508ab3bba64a285302af891c3031
AlexLaidlaw <Alexclaidlaw@gmail.com>
2014-10-16 14:32:39 +0100

    Added a home page, a css file, edited grunt to include uglify and added a gradient to the styling of the page.

---
+++ Portfolio/js/Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        mangle: false
+      },
+      build: {
+        src: '',
+        dest: ''
+      }
+    }
+  });
+
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+
+};
